950551a4d456d8932ade5f02c551be140360a0db
Jozwiak Lukasz (Consultant) <lukasz.jozwiak@external.nexigroup.com>
2023-08-23 08:08:58 +0200

    Init project

---
+++ src/main/java/com/gft/jibmaventest/JibMavenTestApplication.java
@@ -0,0 +1,13 @@
+package com.gft.jibmaventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibMavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibMavenTestApplication.class, args);
+	}
+
+}
